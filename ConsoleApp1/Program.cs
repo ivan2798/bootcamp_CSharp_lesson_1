355a9a4125c7176bf11dde2776f6670675dbd34f
ivan2798 <inge1998@hotmail.com>
2022-10-14 13:11:46 -0500

    Primera aplicación

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Hello World! program
+namespace ConsoleApp1
+{
+    class Hello
+    {
+        // intellisense
+        static void Main(string[] args)
+        {
+            //Omitir System
+            //System.Console.WriteLine("Bienvenidos al bootcamp de C#");
+            Console.WriteLine("Bienvenidos a C#");
+        }
+    }
+}
